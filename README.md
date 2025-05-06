84dd1183735edfe5c2227f139fea5adfeced5fd4
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2025-05-06 12:02:05 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,68 @@
+# JavaScript Nanosecond Timestamp Chart
+
+![JavaScript Nanosecond Timestamp Chart](nanosecondTimestampsChart-darkGold.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
+
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
+
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+Demonstration of time series zoom in capabilities of LightningChart JS.
+
+The example loads random generated time series data with **1 nanosecond time step**.
+To highlight how difficult this is, normal JavaScript numbers can't even accurately measure timestamps with this accuracy.
+Nanosecond timestamps must be loaded as [BigInts](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt).
+
+This example showcases 1 day of data with data gaps and total 1 million data points.
+By default, the user sees the maximum zoomed in view (approximately 50 nanoseconds).
+Below, you can see a zoom band chart showing the complete data set. This is mainly added there to help understand how ridiculous the zoom in magnitude is.
+
+For more details about nanosecond timestamp use cases, please refer to [Developer documentation](https://lightningchart.com/js-charts/docs/more-guides/nanosecond-timestamps/)
+
+
+## API Links
+
+* [XY cartesian chart]
+* [Line series]
+* [Axis]
+* [Axis tick strategies]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [LightningChart][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact sales@lightningchart.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
+
+[0]: https://github.com/Arction/
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://lightningchart.com/support-services/
+
+© LightningChart Ltd 2009-2022. All rights reserved.
+
+
+[XY cartesian chart]: https://lightningchart.com/js-charts/api-documentation/v7.1.0/classes/ChartXY.html
+[Line series]: https://lightningchart.com/js-charts/api-documentation/v7.1.0/classes/LineSeries.html
+[Axis]: https://lightningchart.com/js-charts/api-documentation/v7.1.0/classes/Axis.html
+[Axis tick strategies]: https://lightningchart.com/js-charts/api-documentation/v7.1.0/variables/AxisTickStrategies.html
+
---
+++ src/index.js
@@ -0,0 +1,97 @@
+const lcjs = require('@lightningchart/lcjs')
+const { AxisTickStrategies, emptyFill, lightningChart, Themes } = lcjs
+
+const exampleContainer = document.getElementById('chart') || document.body
+if (exampleContainer === document.body) {
+    exampleContainer.style.width = '100vw'
+    exampleContainer.style.height = '100vh'
+    exampleContainer.style.margin = '0px'
+}
+const containerChart1 = document.createElement('div')
+const containerChart2 = document.createElement('div')
+exampleContainer.append(containerChart1)
+exampleContainer.append(containerChart2)
+containerChart1.style.width = '100%'
+containerChart1.style.height = '80%'
+containerChart2.style.width = '100%'
+containerChart2.style.height = '20%'
+
+//
+// Example data generation
+//
+// step between data points = 1 nanosecond
+const stepNs = BigInt(1)
+// total generated data set time range = 24 hours
+const dateRangeNs = BigInt(24) * BigInt(60) * BigInt(60) * BigInt(1000) * BigInt(1000) * BigInt(1000)
+const sampleCount = 1_000_000
+const data = {
+    x: new BigInt64Array(sampleCount),
+    y: [Number.NaN],
+}
+data.x[0] = BigInt('1741946159532000000') // ns big int timestamp
+let x = data.x[0] + stepNs
+let iArr = 1
+const groupCount = 10
+for (let i = 0; i < groupCount; i += 1) {
+    for (let c = 0; c < Math.floor(sampleCount / groupCount); c += 1) {
+        data.x[iArr] = x
+        data.y[iArr] = Math.random()
+        iArr++
+        x += stepNs
+    }
+    data.x[iArr] = x
+    data.y[iArr] = Number.NaN
+    iArr++
+    x += stepNs
+    x += dateRangeNs / BigInt(groupCount)
+}
+data.y.length = data.x.length
+console.log('raw data', data)
+//
+// Actual nanosecond timestamp chart creation
+//
+const lc = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
+const chart = lc
+    .ChartXY({
+        container: containerChart1,
+        defaultAxisX: {
+            type: 'linear-highPrecision',
+        },
+        theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
+    })
+    .setTitle('Nanosecond timestamp chart')
+
+// NOTE: LCJS can't directly consume bigint64array
+// shift bigint64 data to start from near 0 in order to store it in float64array without losing nanosecond precision
+const xsShifted = new Float64Array(sampleCount)
+const shiftDate = new Date(Number(data.x[0] / BigInt('1000000')))
+shiftDate.setHours(0, 0, 0, 0)
+const shiftOrigin = shiftDate.getTime()
+const shiftOriginNanos = BigInt(shiftOrigin) * BigInt(1_000_000)
+for (let i = 0; i < sampleCount; i++) {
+    xsShifted[i] = Number(data.x[i] - shiftOriginNanos) / 1_000_000
+}
+console.log('shifted data', xsShifted)
+
+const series = chart
+    .addPointLineAreaSeries({ dataPattern: 'ProgressiveX' })
+    .setAreaFillStyle(emptyFill)
+    .setStrokeStyle((stroke) => stroke.setThickness(-1))
+    .appendSamples({ xValues: xsShifted, yValues: data.y })
+
+chart.axisX
+    .setTickStrategy(AxisTickStrategies.DateTime, (strategy) =>
+        strategy
+            // This will result in library accounting for the previous shift by first timestamp
+            .setDateOrigin(shiftDate),
+    )
+    // Apply default view for demo purposes. Note that these are shifted millisecond timestamps.
+    .setInterval({ start: 86159532.54587169, end: 86159532.54595785 })
+
+// Add ZBC just to show the absurd zoom in capability
+const zoomBandChart = lc.ZoomBandChart({ container: containerChart2 })
+zoomBandChart.add(series)
+
+// For more details refer to nanosecond timestamps section in developer documentation https://lightningchart.com/js-charts/docs/
